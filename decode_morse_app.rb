62f1e53d8905c004dd8b14f85b88ccd66502821b
otmanTR <r.brk.otman@gmail.com>
2023-04-13 10:58:48 +0300

    update decode_morse_app.rb file

--- decode_morse_app.rb
+++ decode_morse_app.rb
@@ -1,39 +1,38 @@
-@morse_alphabet = {
-  '.-' => 'A',
-  '-...' => 'B',
-  '-.-.' => 'C',
-  '-..' => 'D',
-  '.' => 'E',
-  '..-.' => 'F',
-  '--.' => 'G',
-  '....' => 'H',
-  '..' => 'I',
-  '.---' => 'J',
-  '-.-' => 'K',
-  '.-..' => 'L',
-  '--' => 'M',
-  '-.' => 'N',
-  '---' => 'O',
-  '.--.' => 'P',
-  '--.-' => 'Q',
-  '.-.' => 'R',
-  '...' => 'S',
-  '-' => 'T',
-  '..-' => 'U',
-  '...-' => 'V',
-  '.--' => 'W',
-  '-..-' => 'X',
-  '-.--' => 'Y',
-  '--..' => 'Z'
-}
-
 # decoding a character using with mapping hashes
 
 def get_char(char)
-  @morse_alphabet[char].upcase
+  morse_alphabet = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_alphabet[char]
 end
 
-get_char('.-')
+puts get_char('.-')
 
 # decoding a word using with get_char and mapping hashes
 
@@ -41,7 +40,7 @@ def get_word(word)
   word.split.map { |char| get_char(char) }.join
 end
 
-get_word('-- -.--   -. .- -- .')
+puts get_word('-- -.--   -. .- -- .')
 
 # decoding a santences with using get_sentences, get_word and mapping hashes
 
@@ -49,4 +48,4 @@ def get_sentences(sentence)
   sentence.split('  ').map { |word| get_word(word) }.join('  ')
 end
 
-get_sentences('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
+puts get_sentences('      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
